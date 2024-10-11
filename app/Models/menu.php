cd7406da78709403d70281ba290d14d022553d3f
Ferdinand <ferdinandsianturi28@gmail.com>
2024-10-11 22:10:10 +0700

    menu

--- app/Models/menu.php
+++ app/Models/menu.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class menu extends Model
+class Menu extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['nama_menu', 'size', 'harga'];
 }
